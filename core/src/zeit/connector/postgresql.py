0193f3af9669121701973f8c62576d28d83daacc
Florian Schulze <mail@florian-schulze.net>
2021-11-26 12:42:35 +0100

    WIP

--- core/src/zeit/connector/postgresql.py
+++ core/src/zeit/connector/postgresql.py
@@ -3,17 +3,16 @@ from lazy import lazy
 from lxml.html import fromstring as parse_html
 from sqlalchemy import MetaData
 from sqlalchemy import Column
-from sqlalchemy import ForeignKey
 from sqlalchemy import LargeBinary
 from sqlalchemy import Unicode
 from sqlalchemy import TIMESTAMP
 from sqlalchemy import create_engine
 from sqlalchemy import event
+from sqlalchemy import schema
+from sqlalchemy import sql
 from sqlalchemy.dialects.postgresql import JSONB
 from sqlalchemy.orm import Mapper
-from sqlalchemy.orm import backref
 from sqlalchemy.orm import declarative_base
-from sqlalchemy.orm import relationship
 from sqlalchemy.orm import scoped_session
 from sqlalchemy.orm import sessionmaker
 from zeit.connector.interfaces import DeleteProperty
@@ -71,38 +70,34 @@ BaseObject = declarative_base(metadata=metadata_object)
 class StorageItem(BaseObject):
     __tablename__ = "storage"
 
-    path = Column(Unicode, primary_key=True)
+    parent_path = Column(Unicode, primary_key=True, index=True)
+    id = Column(Unicode, primary_key=True)
     blob = Column(LargeBinary, nullable=False)
     properties = Column(JSONB, nullable=False)
 
     def __repr__(self):
-        return f"<{self.__class__.__name__} path={self.path}>"
-
-
-class CanonicalPath(BaseObject):
-    __tablename__ = "canonical_path"
-
-    id = Column(Unicode, primary_key=True)
-    path = Column(
-        Unicode,
-        ForeignKey(StorageItem.path, onupdate="CASCADE", ondelete="CASCADE"),
-        nullable=True)
-    item = relationship(StorageItem, lazy='joined', backref=backref('cpaths', lazy='joined'))
-
-    def __repr__(self):
-        return f"<{self.__class__.__name__} id={self.id} path={self.path}>"
+        return f"<{self.__class__.__name__} parent_path={self.parent_path} id={self.id}>"
 
 
 class Lock(BaseObject):
     __tablename__ = "locks"
 
-    path = Column(
+    parent_path = Column(
         Unicode,
-        ForeignKey(StorageItem.path, onupdate="CASCADE", ondelete="CASCADE"),
+        primary_key=True)
+    id = Column(
+        Unicode,
         primary_key=True)
     principal = Column(Unicode, nullable=False)
     until = Column(TIMESTAMP(timezone=True), nullable=False)
     token = Column(Unicode, nullable=False)
+
+    __table_args__ = (
+        schema.ForeignKeyConstraint(
+            (parent_path, id),
+            (StorageItem.parent_path, StorageItem.id),
+            onupdate="CASCADE", ondelete="CASCADE"),
+    )
 
 
 def _get_resource_type(properties):
@@ -311,7 +306,22 @@ class Connector(object):
     def _id2path(self, id):
         if not id.startswith(self._prefix):
             raise ValueError("Bad id %r (prefix is %r)" % (id, self._prefix))
-        return id[len(self._prefix):]
+        return id[len(self._prefix):].rstrip('/')
+
+    def _id2key(self, id):
+        if not id.startswith(self._prefix):
+            raise ValueError("Bad id %r (prefix is %r)" % (id, self._prefix))
+        path = self._id2path(id)
+        if not path:
+            # root object
+            # print(f"    _id2key {id} root")
+            return ('', '')
+        result = path.rsplit('/', 1)
+        if len(result) == 1:
+            # root level objects
+            result = ['', *result]
+        # print(f"    _id2key {id} {result}")
+        return result
 
     @lazy
     def DAVConnector(self):
@@ -319,60 +329,47 @@ class Connector(object):
         return zeit.connector.connector.Connector
 
     def listCollection(self, id):
-        # if id != self._prefix:
-        #     import pdb; pdb.set_trace()
-        try:
-            obj = self[id]
-        except KeyError:
-            print("    listCollection", id, "Not Found")
-            return []
-        body = obj.data.read()
-        if not body:
-            print("    listCollection", id, [])
-            return []
-        tree = parse_html(body)
-        result = sorted(
-            (x.text.rstrip('/'), os.path.join(id, x.text))
-            for x in tree.cssselect('td > a')
-            if x.text != 'Parent Directory' and not x.attrib['href'].startswith('/'))
+        path = self._id2path(id)
+        session = DBSession()
+        ids = session.execute(
+            sql.select(StorageItem.id)
+            .where(StorageItem.parent_path == path, StorageItem.id != ''))
+        result = [(x.id, os.path.join(id, x.id)) for x in ids]
         print("    listCollection", id, result)
         return result
 
     def __contains__(self, id):
         session = DBSession()
-        cpath = session.get(CanonicalPath, id)
-        result = cpath is not None
+        item = session.get(StorageItem, self._id2key(id))
+        result = item is not None
         print("    contains", id, result)
         return result
 
     def __delitem__(self, id):
         print("    delitem", id)
         session = DBSession()
-        cpath = session.get(CanonicalPath, id)
-        if cpath is None:
+        item = session.get(StorageItem, self._id2key(id))
+        if item is None:
             raise KeyError(
                 "The resource %r does not exist." % six.text_type(id))
-        for obj in (*cpath.item.cpaths, cpath.item):
-            session.delete(obj)
+        session.delete(item)
 
     def __getitem__(self, id):
-        print("    getitem", id)
+        # print("    getitem", id)
         # if id != self._prefix:
         #     import pdb; pdb.set_trace()
         session = DBSession()
-        cpath = session.get(CanonicalPath, id)
-        if cpath is None:
+        item = session.get(StorageItem, self._id2key(id))
+        if item is None:
             raise KeyError(
                 "The resource %r does not exist." % six.text_type(id))
-        item = cpath.item
         properties = _convert_properties_from_dict(item.properties)
         # if id != self._prefix:
         #     import pdb; pdb.set_trace()
         resource_type = _get_resource_type(properties)
         content_type = properties.get(('getcontenttype', 'DAV:'))
         body = BytesIO(item.blob)
-        path = item.path
-        cid = self._prefix + path
+        cid = os.path.join(self._prefix, item.parent_path, item.id)
         result = zeit.connector.resource.CachedResource(
             six.text_type(cid),
             self.DAVConnector._id_splitlast(cid)[1].rstrip('/'),
@@ -385,99 +382,69 @@ class Connector(object):
         return result
 
     def add(self, obj, verify_etag=True):
-        print(f"    add {obj.id} {obj.contentType} {obj.type}")
-        # if obj.id != self._prefix:
-        #     import pdb; pdb.set_trace()
-        path = self._id2path(obj.id)
+        key = self._id2key(obj.id)
         metadata = _convert_properties_to_dict(obj.properties)
         metadata.setdefault('DAV:', {}).setdefault('getcontenttype', obj.contentType)
-        metadata.setdefault('DAV:', {}).setdefault('getlastmodified', datetime.datetime.now().strftime(RFC3339_FORMAT))
+        metadata.setdefault('DAV:', {}).setdefault('getlastmodified', datetime.datetime.now(tz=datetime.timezone.utc).strftime(RFC3339_FORMAT))
         metadata.setdefault(RESOURCE_TYPE_PROPERTY[1], {}).setdefault(RESOURCE_TYPE_PROPERTY[0], obj.type)
         if hasattr(obj.data, 'seek'):
             obj.data.seek(0)
         body = obj.data.read()
         session = DBSession()
-        cpath = session.get(CanonicalPath, obj.id)
-        if cpath is not None:
+        item = session.get(StorageItem, key)
+        if item is not None:
             print(f"    updated existing {obj.id} {obj.contentType} {obj.type}")
             # update existing item
-            cpath.item.body = body
-            cpath.item.properties.update(metadata)
-            _remove_deleted_metadata(cpath.item.properties)
+            item.body = body
+            item.properties.update(metadata)
+            _remove_deleted_metadata(item.properties)
             return
         # we need to add a new item
         print(f"    new {obj.id} {obj.contentType} {obj.type}")
         session.add(StorageItem(
-            path=path,
+            parent_path=key[0],
+            id=key[1],
             blob=body,
             properties=metadata))
-        session.add(CanonicalPath(id=obj.id, path=path))
-        if obj.id.endswith('/'):
-            cid = obj.id.rstrip('/')
-        else:
-            cid = obj.id + '/'
-        if cid != obj.id:
-            session.add(CanonicalPath(id=cid, path=path))
-        (parent_id, child_id) = self.DAVConnector._id_splitlast(cid)
-        if parent_id.endswith('://'):
-            # the obj is the root
-            return
-        children = [x[0] for x in self.listCollection(parent_id)]
-        parent_path = session.get(CanonicalPath, parent_id)
-        if parent_path is None:
-            import pdb; pdb.set_trace()
-        print(f"    adding child {child_id} to {parent_id}")
-        parent_path.item.blob = b'\n'.join(
-            ('<td><a href="%s">%s</a></td>' % (x, x.rstrip('/'))).encode('utf-8')
-            for x in sorted(set(children + [child_id])))
 
     def changeProperties(self, id, properties, locktoken=None):
         print(f"    changeProperties {id}")
         properties.pop(zeit.connector.interfaces.UUID_PROPERTY, None)
         metadata = _convert_properties_to_dict(properties)
         session = DBSession()
-        cpath = session.get(CanonicalPath, id)
-        cpath.item.properties.update(metadata)
-        _remove_deleted_metadata(cpath.item.properties)
+        item = session.get(StorageItem, self._id2key(id))
+        item.properties.update(metadata)
+        _remove_deleted_metadata(item.properties)
 
     def copy(self, old_id, new_id):
         import pdb; pdb.set_trace()
 
     def move(self, old_id, new_id):
         print(f"    move {old_id} {new_id}")
-        session = DBSession()
-        old_cpath = session.get(CanonicalPath, old_id)
+        old_path = self._id2path(old_id)
+        old_key = self._id2key(old_id)
         new_path = self._id2path(new_id)
-        old_cpath.item.path = new_path
-        for cpath in old_cpath.item.cpaths:
-            if cpath.id == old_id + '/':
-                cpath.id = new_id + '/'
-                cpath.path = new_path
-            elif cpath.id == old_id.rstrip('/'):
-                cpath.id = new_id.rstrip('/')
-                cpath.path = new_path
-        (old_parent_id, old_child_id) = self.DAVConnector._id_splitlast(old_id)
-        old_children = [x[0] for x in self.listCollection(old_parent_id)]
-        old_parent_path = session.get(CanonicalPath, old_parent_id)
-        print(f"    removing child {old_child_id} from {old_parent_id}")
-        old_parent_path.item.blob = b'\n'.join(
-            ('<td><a href="%s">%s</a></td>' % (x, x.rstrip('/'))).encode('utf-8')
-            for x in sorted(set(x for x in old_children if x != old_child_id)))
-        (new_parent_id, new_child_id) = self.DAVConnector._id_splitlast(new_id)
-        new_children = [x[0] for x in self.listCollection(new_parent_id)]
-        new_parent_path = session.get(CanonicalPath, new_parent_id)
-        print(f"    adding child {new_child_id} to {new_parent_id}")
-        new_parent_path.item.blob = b'\n'.join(
-            ('<td><a href="%s">%s</a></td>' % (x, x.rstrip('/'))).encode('utf-8')
-            for x in sorted(set(new_children + [new_child_id])))
+        new_key = self._id2key(new_id)
+        session = DBSession()
+        old_item = session.get(StorageItem, old_key)
+        old_item.parent_path = new_key[0]
+        old_item.id = new_key[1]
+        session.execute(
+            sql.update(StorageItem)
+            .where(StorageItem.parent_path == old_path)
+            .values(parent_path=new_path))
 
     def lock(self, id, principal, until):
         # XXX does locking update getlastmodified DAV property?
         print("    lock", id, principal, until)
+        if not until:
+            until = datetime.datetime.now(tz=datetime.timezone.utc)
         session = DBSession()
         token = secrets.token_hex()
+        key = self._id2key(id)
         session.add(Lock(
-            path=self._id2path(id),
+            parent_path=key[0],
+            id=key[1],
             principal=principal,
             until=until,
             token=token))
@@ -485,7 +452,7 @@ class Connector(object):
 
     def locked(self, id):
         session = DBSession()
-        lock = session.get(Lock, self._id2path(id))
+        lock = session.get(Lock, self._id2key(id))
         if lock is None:
             result = (None, None, False)
             print("    locked", id, result)
@@ -512,7 +479,7 @@ class Connector(object):
     def unlock(self, id, locktoken=None, invalidate=True):
         print("    unlock", id, locktoken, invalidate)
         session = DBSession()
-        lock = session.get(Lock, self._id2path(id))
+        lock = session.get(Lock, self._id2key(id))
         if lock is None:
             import pdb; pdb.set_trace()
         else:
